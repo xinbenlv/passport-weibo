ca3a62b50489d139c63b673b2189dc16914ca0bd
Zainan Victor Zhou <zzn@zzn.im>
2020-01-19 21:16:32 -0800

    Upgrade dependencies for fixes #43, fixes #44, fixes 12, Part of #45

--- test/index-test.js
+++ test/index-test.js
@@ -1,15 +1,15 @@
 var vows = require('vows');
 var assert = require('assert');
 var util = require('util');
-var github = require('passport-weibo');
+var weibo = require('passport-weibo');
 
 
 vows.describe('passport-weibo').addBatch({
-  
+
   'module': {
     'should report a version': function (x) {
-      assert.isString(github.version);
+      assert.isString(weibo.version);
     },
   },
-  
+
 }).export(module);
